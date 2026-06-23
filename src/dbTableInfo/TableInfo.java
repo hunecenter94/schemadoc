c99046fe30785ae0c033a8124ede4f358cbdafdc
RubikCube <70802654+alsrbxo0428@users.noreply.github.com>
2026-06-23 09:56:03 +0900

    Update TableInfo.java

--- src/dbTableInfo/TableInfo.java
+++ src/dbTableInfo/TableInfo.java
@@ -50,7 +50,7 @@ public class TableInfo {
 	 * <p>VIEW 조회시 isSelectView: true</p>
 	 * @return TABLE_NAME, COMMENTS
 	 */
-	public static String getTableInfoSQL(String dbType, String includeView, String dbName) {
+	public static String getTableInfoSQL(String dbType, String isSelectView, String dbName) {
 		StringBuilder sql = new StringBuilder();
 		
 		if(DbTableInfo.Oracle.equals(dbType)) {	// ORACLE
@@ -63,7 +63,7 @@ public class TableInfo {
 			sql	.append(" ( ")
 				.append("	SELECT TABLE_NAME, 'TABLE' AS TABLE_TYPE FROM USER_TABLES ");
 			
-			if("Y".equals(includeView)) {
+			if("Y".equals(isSelectView)) {
 				sql	.append("	UNION ALL ")
 					.append("	SELECT VIEW_NAME, 'VIEW' AS TABLE_TYPE FROM USER_VIEWS ");
 			}
@@ -83,41 +83,40 @@ public class TableInfo {
 				.append(" WHERE ")
 				.append("	TABLE_SCHEMA = DATABASE() ");
 			
-			if(!"Y".equals(includeView)) {
+			if(!"Y".equals(isSelectView)) {
 				sql.append(" AND TABLE_TYPE = 'BASE TABLE' ");
 			}
 			
 			sql	.append(" ORDER BY TABLE_NAME ");
 		}else if(DbTableInfo.PostgreSQL.equals(dbType)) { //PostgreSQL
 			sql	.append(" select ")
-				.append("     tb.relname as table_name, ")
-				.append("     tb.table_type, ")
-				.append("     tb_dc.description ")
+				.append(" 	tb.relname as table_name, ")
+				.append(" 	tb.table_type, ")
+				.append(" 	tb_dc.description ")
 				.append(" from ")
-				.append("     ( ")
-				.append("         select ")
-				.append("             pc.relname, ")
-				.append("             pc.oid, ")
-				.append("             case ")
-				.append("                 when tb.table_type = 'BASE TABLE' then 'TABLE' ")
-				.append("                 when tb.table_type = 'VIEW' then 'VIEW' ")
-				.append("             end as table_type ")
-				.append("         from ")
-				.append("             pg_catalog.pg_class pc ")
-				.append("             join information_schema."tables" tb ")
-				.append("                 on tb.table_schema = '").append(dbName).append("' ")
-				.append("                 and pc.relname = tb.table_name ")
-				.append("     ) tb ")
-				.append("     left join pg_description tb_dc ")
-				.append("         on tb_dc.objsubid = 0 and tb.oid = tb_dc.objoid ");
-			
-			if(!"Y".equals(includeView)) {
-				sql	.append(" where ")
-					.append("     tb.table_type = 'BASE TABLE' ");
+				.append(" 	( ")
+				.append(" 		select ")
+				.append(" 			pc.relname, ")
+				.append(" 			pc.oid, ")
+				.append(" 			case ")
+				.append(" 				when tb.table_type = 'BASE TABLE' then 'TABLE' ")
+				.append(" 				when tb.table_type = 'VIEW' then 'VIEW' ")
+				.append(" 			end as table_type ")
+				.append(" 		from ")
+				.append(" 			pg_catalog.pg_class pc ")
+				.append(" 			join information_schema.tables tb ")
+				.append(" 				on tb.table_schema = '").append(dbName).append("' ")
+				.append(" 				and pc.relname = tb.table_name ");
+
+			if(!"Y".equals(isSelectView)) {
+				sql.append(" 			and tb.table_type = 'BASE TABLE' ");
 			}
-			
-			sql	.append(" order by ")
-				.append("     tb.relname ");
+
+			sql	.append(" 	) tb ")
+				.append(" 	left join pg_description tb_dc ")
+				.append(" 		on tb_dc.objsubid = 0 and tb.oid = tb_dc.objoid ")
+				.append(" order by ")
+				.append(" 		tb.relname ");
 		}
 		
 		System.out.println(sql.toString());
